ade77638b8dd101b203e3d1cedcf563baa82636d
v1rtl <pilll.pl22@gmail.com>
2020-08-26 22:39:00 +0300

    Move back to esbuild because of a strange esbuild output

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import ts from 'rollup-plugin-typescript2'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+    },
+  ],
+  plugins: [ts()],
+  external: ['path', 'mime-db'],
+}
