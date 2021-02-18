b2ee9f32c80434a1e10456f5716fe7fe2eabe6f9
Alistair Smith <alistairsmith01@gmail.com>
2021-02-18 14:40:05 +0000

    remove cjs from rollup config
    Node 10 is EOL

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,6 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.cjs',
-      format: 'cjs',
-    },
-    {
       file: 'dist/index.js',
       format: 'esm',
     },
